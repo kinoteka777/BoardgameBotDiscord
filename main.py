c73323b084983dcf808f72f69b8acbaaec4d0b56
Casey <134029040+kinoteka777@users.noreply.github.com>
2025-03-21 16:10:41 +1030

    refactored code to use pycord

--- main.py
+++ main.py
@@ -1,57 +1,27 @@
 from typing import Final
 import os
 from dotenv import load_dotenv
-from discord import Intents, Client, Message
-from responses import get_response
+import discord
 
 # LOAD TOKEN
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 
 # BOT SETUP
-intents: Intents = Intents.default()
-intents.message_content = True
-client: Client = Client(intents=intents)
+bot = discord.Bot()
 
-# MESSAGE FUNCTIONALITY
-async def send_message(message: Message, user_message: str) -> None:
-    if not user_message:
-        print('Empty message')
-        return
-    if user_message[0] != '!': # if command does not begin with '!', it will not be recognised as a command
-        return
-        
-    user_message = user_message[1:]
-    if is_private := user_message[0] == '?':
-        user_message = user_message[1:]
-
-    try:
-        response: str = get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response) # Sends message to user via DM if is_private = true, otherwise sends it to channel
-    except Exception as e:
-        print(e)
+@bot.slash_command(name="ping", description="Shows the bot\'s latency.")
+async def ping(ctx: discord.ApplicationContext):
+    await ctx.respond(f"Pong! latency is {bot.latency}")
 
 # STARTUP HANDLING
-@client.event
+@bot.event
 async def on_ready() -> None:
-    print(f'{client.user} is now running!')
-
-# INCOMING MESSAGE HANDLING
-@client.event
-async def on_message(message: Message) -> None:
-    if message.author == client.user: # prevents bot from responding to itself
-        return
-    
-    username: str = str(message.author)
-    user_message: str = message.content
-    channel: str = str(message.channel)
-
-    print(f'[{channel}] {username}: "{user_message}"')
-    await send_message(message, user_message)
+    print(f'{bot.user} is now running!')
 
 # MAIN ENTRY POINT
 def main() -> None:
-    client.run(token=TOKEN)
+    bot.run(token=TOKEN)
 
 if __name__ == '__main__':
     main()
